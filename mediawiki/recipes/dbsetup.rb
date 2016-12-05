b4fdbb4a18cb6fd739fd4f56cbd695155b2f9bdf
Steve Hogg <steve@collecto.io>
2016-12-06 11:18:38 +1300

    try to add sql dump to file system

--- mediawiki/recipes/dbsetup.rb
+++ mediawiki/recipes/dbsetup.rb
@@ -1,6 +1,12 @@
 node[:deploy].each do |app_name, deploy|
+  cookbook_file '/tmp/mediawiki.sql.gz' do
+    source 'mediawiki.sql.gz'
+    mode '0400'
+    action :create
+  end
+
   execute "mysql-setup" do
-    command "cat mediawiki.sql.gz | gzip -d | /usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
+    command "cat /tmp/mediawiki.sql.gz | gzip -d | /usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
     not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:mediawiki][:checktable]}"
     action :run
   end
