addfb23d2dea24be017e2eed141829c8bd21e552
Steve Hogg <steve@collecto.io>
2016-12-06 11:03:33 +1300

    try relative path to SQL dump

--- mediawiki/recipes/dbsetup.rb
+++ mediawiki/recipes/dbsetup.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-setup" do
-    command "cat /assets/mediawiki.sql.gz | gzip -d | /usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
+    command "cat mediawiki.sql.gz | gzip -d | /usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]}"
     not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:mediawiki][:checktable]}"
     action :run
   end
