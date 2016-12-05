251b9cffa8cbc9f843200741ecb79b3b3b599c21
Steve Hogg <steve@collecto.io>
2016-12-05 16:54:07 +1300

    add host to MySQL connection string

--- mediawiki/recipes/dbsetup.rb
+++ mediawiki/recipes/dbsetup.rb
@@ -1,12 +1,12 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-create-table" do
-    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:mediawiki][:dbtable]}(
+    command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:mediawiki][:dbtable]}(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     author VARCHAR(63) NOT NULL,
     message TEXT,
     PRIMARY KEY (id)
   )'"
-    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:mediawiki][:dbtable]}"
+    not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:mediawiki][:dbtable]}"
     action :run
   end
 end
