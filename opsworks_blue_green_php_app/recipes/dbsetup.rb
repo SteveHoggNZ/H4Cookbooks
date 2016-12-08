380c866d66e5cd7237e2a3b4d6a6a39265fa0cc8
Steve Hogg <steve@collecto.io>
2016-12-08 14:09:17 +1300

    insert default messages for opsworks_blue_green_php_app

--- opsworks_blue_green_php_app/recipes/dbsetup.rb
+++ opsworks_blue_green_php_app/recipes/dbsetup.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |app_name, deploy|
   execute "mysql-setup" do
-    command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e 'CREATE TABLE #{node[:app][:table]} (id int primary key, message text)'"
+    command "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e \"CREATE TABLE #{node[:app][:table]} (id int primary key, message text) ; INSERT INTO messages VALUES (1, 'Hello World'), (2, 'My second message')\""
     not_if "/usr/bin/mysql -h#{deploy[:database][:host]} -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:app][:table]}"
     action :run
   end
